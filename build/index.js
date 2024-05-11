47d4ef0ae55847aad69eb2b6e65460a8afc7d15d
Arkadii Shvartcman <a.shvartcman@gmail.com>
2024-05-11 13:21:56 +0200

    remove build from git

--- build/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tracker_1 = require("./tracker");
-(0, tracker_1.getTroughGroup)(process.argv[2].split(",").filter((el) => el.length), process.argv[3].split(",").filter((el) => el.length));
